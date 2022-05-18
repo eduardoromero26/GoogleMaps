31dc8722310a9496102e5b71cb13e7bf22db6fe2
Raziel Cervantes <razielcervantesromero@gmail.com>
2022-05-18 18:56:49 -0500

    adding the option to display the current location

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
+  // late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  // final LatLng _center = const LatLng(45.521563, -122.677433);
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  // void _onMapCreated(GoogleMapController controller) {
+  //   mapController = controller;
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: MyHomePage()
+
+        // Scaffold(
+        //   appBar: AppBar(
+        //     title: const Text('Maps Sample App'),
+        //     backgroundColor: Colors.green[700],
+        //   ),
+        //   body: GoogleMap(
+        //     onMapCreated: _onMapCreated,
+        //     initialCameraPosition: CameraPosition(
+        //       target: _center,
+        //       zoom: 11.0,
+        //     ),
+        //   ),
+        // ),
+        );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -9,8 +12,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var lat = 0.0;
-  var lng = 0.0;
+  double lat = 0.0;
+  double lng = 0.0;
 
   findMyLocation() async {
     bool devicePermission;
@@ -24,13 +27,18 @@ class _MyHomePageState extends State<MyHomePage> {
       }
       var location = await Geolocator.getCurrentPosition();
       print(location.accuracy);
+
       setState(() {
         lat = location.latitude;
         lng = location.longitude;
       });
+      print(lat);
+      print(lng);
     } else {
       print("Error : GPS sensor permission issue, device level");
     }
+
+    final LatLng _center = LatLng(lat, lng);
   }
 
   @override
@@ -39,6 +47,16 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  Completer<GoogleMapController> _controller = Completer();
+
+  late CameraPosition initialPoint = CameraPosition(
+      target: LatLng(34.47554013447298, -100.6012667768258),
+      tilt: 59.440717697143555,
+      zoom: 3);
+
+  late CameraPosition mylocation = CameraPosition(
+      target: LatLng(lat, lng), tilt: 59.440717697143555, zoom: 20);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,9 +64,24 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Google Maps'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text("${lng.toString()} lng,  ${lat.toString()} Lat"),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToMyLocation,
+        label: const Text("my location"),
+        icon: Icon(Icons.my_location),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
+  }
+
+  Future<void> _goToMyLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(mylocation));
   }
 }
