09557bf8914c141fb627d53eda92061683bccc7b
eduardoromero26 <edu.roc26@gmail.com>
2022-05-18 12:39:11 -0500

    SFind my Location was added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-
-
+import 'package:geolocator/geolocator.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -11,13 +9,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var lat = 0.0;
+  var lng = 0.0;
 
+  findMyLocation() async {
+    bool devicePermission;
+    devicePermission = await Geolocator.isLocationServiceEnabled();
+    if (devicePermission) {
+      var appLevel = await Geolocator.checkPermission();
+      if (appLevel == LocationPermission.denied) {
+        appLevel = await Geolocator.requestPermission();
+      } else if (appLevel == LocationPermission.deniedForever) {
+        print("Error: App location permission denied forever");
+      }
+      var location = await Geolocator.getCurrentPosition();
+      print(location.accuracy);
+      setState(() {
+        lat = location.latitude;
+        lng = location.longitude;
+      });
+    } else {
+      print("Error : GPS sensor permission issue, device level");
+    }
+  }
+
+  @override
+  initState() {
+    findMyLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Google Maps'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("${lng.toString()} lng,  ${lat.toString()} Lat"),
       ),
     );
   }
